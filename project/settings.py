6d2791600d14518ee545b677d61c30fe1ba93c75
fatimatoghoz23 <140913550+fatimatoghoz23@users.noreply.github.com>
2024-01-03 22:29:23 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -124,9 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'project/static')
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 AUTH_USER_MODEL = 'pages.User'
