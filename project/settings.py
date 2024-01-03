daee7744152d83df57aa80d6fb69429683f0c9e7
fatimatoghoz23 <140913550+fatimatoghoz23@users.noreply.github.com>
2024-01-03 20:31:03 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS=[
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+AUTH_USER_MODEL = 'pages.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
