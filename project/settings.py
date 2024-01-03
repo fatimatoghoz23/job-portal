52228202d3218651de11c00158d8ddfd806bc867
fatimatoghoz23 <140913550+fatimatoghoz23@users.noreply.github.com>
2024-01-03 20:25:43 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'pages.apps.PagesConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
